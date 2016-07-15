8b7f60a7f19648934bc44a55c193d4d5a38c7231
Midi Kang <midi13@163.com>
2016-07-15 17:41:44 +0800

    Validation! test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -52,4 +52,14 @@ class ProductsControllerTest < ActionController::TestCase
 
     assert_redirected_to products_path
   end
+  
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title: products(:ruby).title,
+    description: "yyy",
+    price: 1,
+    image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')],
+    product.errors[:title]
+  end
 end
